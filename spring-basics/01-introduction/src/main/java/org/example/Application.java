940d949c6bd1d16b7836af0d141a57cc8a6569df
bakka <kacper.bak@pentasys.de>
2014-05-17 13:30:16 +0200

    package refactored to org.example

--- spring-basics/01-introduction/src/main/java/org/example/Application.java
+++ spring-basics/01-introduction/src/main/java/org/example/Application.java
@@ -10,7 +10,7 @@ public class Application {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("de/kacperbak/spring.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("org/example/spring.xml");
 
         BookingService service = context.getBean("jodaService", BookingServiceImpl.class);
 
